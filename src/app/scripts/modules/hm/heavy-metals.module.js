8ee39abd4c09212a2d09edd3823f02157f3c81e0
Sanil Naik <sanil.m.naik@gmail.com>
2015-06-15 23:48:43 +0530

    Local storage included

---
+++ src/app/scripts/modules/hm/heavy-metals.module.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @author Example <sanil.m.naik@gmail.com>
+ * @copyright 2015 NobleIron. All rights reserved.
+ */
+
+/**
+ * @namespace HeavyMetals
+ */
+
+(function () {
+
+  angular.module('HeavyMetals',
+    ['ngAnimate',
+      'ngCookies',
+      'ngTouch',
+      'ngSanitize',
+      'ui.router',
+      'ui.bootstrap',
+      'LocalStorageModule',
+      'HM_ConfigMD',
+      'HM_GlobalMD'])
+
+
+}());
+
