af063699b13a20ff2b24fba9005c9e8ede26c5db
Sanil Naik <sanil.m.naik@gmail.com>
2015-08-12 00:57:03 +0530

    Validation message changes

--- src/app/scripts/constants/validation/validation.constraints.constant.js
+++ src/app/scripts/constants/validation/validation.constraints.constant.js
@@ -2,7 +2,25 @@ angular.module("HM_ConfigMD")
   .constant('ValidationConstraintsCnst', {
 
       'User': {
+        'credentials' : {
+          'required' : {
+            'message' : 'Invalid Credentials'
+          }
+        },
+        'firstName' : {
+          'required' : {
+              'message' : "First Name is required"
+          }
+        },
+        'lastName' : {
+          'required' : {
+            'message' : "Last Name is required"
+          }
+        },
         'email': {
+          'email': {
+            'message' : 'Please enter valid Email'
+          },
           'required': {
             'message': 'Email is required.'
           }
