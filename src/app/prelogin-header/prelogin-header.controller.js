937c67173c383cff81fcc2770f350386095fa42b
Sanil Naik <sanil.m.naik@gmail.com>
2015-11-02 23:47:24 +0530

    search changes on header

--- src/app/prelogin-header/prelogin-header.controller.js
+++ src/app/prelogin-header/prelogin-header.controller.js
@@ -1,74 +1,48 @@
-'use strict';
-/**
- * @ngdoc directive
- * @name rfx.directive:rAutogrow
- * @element textarea
- * @function
- *
- * @description
- * Resize textarea automatically to the size of its text content.
- *
- * **Note:** ie<9 needs pollyfill for window.getComputedStyle
- *
- * @example
- <example module="rfx">
- <file name="index.html">
- <textarea ng-model="text"rx-autogrow class="input-block-level"></textarea>
- <pre>{{text}}</pre>
- </file>
- </example>
- */
 angular.module('HeavyMetals')
-  .controller('HM_PreloginHeaderCtrl', function ($scope,filterFilter) {
-    /**
-     * Represents a book.
-     * @constructor
-     * @param {string} title - The title of the book.
-     * @param {string} author - The author of the book.
-     */
-    $scope.date = new Date();
+  .controller('HM_PreloginHeaderCtrl',['$scope','$state','filterFilter','HM_RestSV','HM_PreloginHeaderCnst',function ($scope,$state,filterFilter, RestSV,HeaderCnst) {
+
+    $scope.getProducts = getProducts;
+
+    $scope.selectSearchedItem = selectSearchedItem;
 
 
-    $scope.selectedUser = '';
+    _initialize();
 
-    var users = [{
-      name: 'Electric Scissor lift',
-      group: 'Equipments'
-    },{
-      name: 'Electric Scissor lift (Wide)',
-      group: 'Equipments'
-    },{
-      name: 'Electric Scissor lift - 16\"',
-      group: 'Equipments'
-    }, {
-      name: 'Electric transmission of gearbox',
-      group: 'Parts'
-    }, {
-      name: 'Electric gear docket',
-      group: 'Parts'
-    }, {
-      name: 'Expansion Electric Chord',
-      group: 'Attachments'
-    }, {
-      name: 'Advanced Electric resistance socket box',
-      group: 'Attachments'
+    function selectSearchedItem(item, model, label){
+      debugger
+      var query = '';
+      if(typeof $scope.selectedProduct == "object"){
+        query = $scope.selectedProduct.Product_Name;
+      }
+      if(typeof $scope.selectedProduct == "string"){
+        query = $scope.selectedProduct;
+      }
+
+      $state.go('hm.search', { query : query},{ reload: true });
     }
-    ];
 
-    $scope.getUsers = function (search) {
-      var filtered = filterFilter(users, search);
-
-      var results = _(filtered)
-        .groupBy('group')
-        .map(function (g) {
-          g[0].firstInGroup = true;  // the first item in each group
-          return g;
-        })
-        .flatten()
-        .value();
-
-      console.log(results);
-
-      return results;
+    function getProducts(search){
+      return RestSV.get( HeaderCnst.search.url() ,{
+        search_text : 'lift'
+      }).then(function(response){
+        var filtered = filterFilter(response.data.result.SearchResult, search);
+        var results = _(filtered)
+          .groupBy('Category_Name')
+          .map(function (g) {
+            g[0].firstInGroup = true;  // the first item in each group
+            return g;
+          })
+          .flatten()
+          .value();
+        return results;
+      });
     }
-  });
+
+    function _initialize(){
+      $scope.selectedProduct = '';
+      $scope.flags = {
+        rented : true
+      }
+    }
+
+  }]);
