361d0c26e7ed77ed537e74fc4275bf021b596391
Sanil Naik <sanil.m.naik@gmail.com>
2015-11-22 23:11:32 +0530

    Forgot password changes

--- src/app/forgot-password/forgot-password.controller.js
+++ src/app/forgot-password/forgot-password.controller.js
@@ -1,7 +1,26 @@
 'use strict';
 angular.module('HM_ForgotPasswordMD')
-  .controller('HM_ForgotPasswordCtrl', ['$scope','$state','HM_loginCnst','HM_RestSV','localStorageService','$http', function ($scope, $state, LoginCnst,RestSV, localStorageService,$http ) {
+  .controller('HM_ForgotPasswordCtrl', ['$scope','$state','HM_ForgotPasswordCnst','HM_RestSV', function ($scope, $state, ForgotPasswordCnst,RestSV ) {
 
 
+    $scope.flags = {
+      forgotPasswordNotifyInitiated : false
+    }
+    $scope.notifyForgotPassword = notifyForgotPassword;
+
+
+    function notifyForgotPassword(){
+      if($scope.forgotPasswordForm.$valid){
+        RestSV
+          .post( ForgotPasswordCnst.forgotPassword.url() ,{
+            email : $scope.forgotPasswordData.email,
+          })
+          .finally(function(response){
+            $scope.flags.forgotPasswordNotifyInitiated = true;
+
+          });
+      }
+      debugger
+    }
 
   }]);
