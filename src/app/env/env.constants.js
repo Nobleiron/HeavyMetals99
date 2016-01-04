28f26a8dd1ef61b3e85bf040c361e42dd9364237
Sanil Naik <sanil.m.naik@gmail.com>
2016-01-04 20:23:02 +0530

    Env changes

--- src/app/env/env.constants.js
+++ src/app/env/env.constants.js
@@ -4,7 +4,7 @@ angular.module("HM_EnvMD")
     current : 'development',
 
     development : {
-      restEndPoint : 'http://52.88.68.50/index.php'
+      restEndPoint : 'http://52.33.175.46/index.php'
     },
 
     staging : {
