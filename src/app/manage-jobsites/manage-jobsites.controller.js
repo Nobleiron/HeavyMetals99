13fe50aef89095997fce43693442624550c0b68d
ankit <ankit@procoefficient.com>
2016-01-08 13:48:49 +0530

    landing page header when logged in box shadow issue reserve equipment filter search on all dashboard pages view jobsite responsive other ui changes

--- src/app/manage-jobsites/manage-jobsites.controller.js
+++ src/app/manage-jobsites/manage-jobsites.controller.js
@@ -15,4 +15,28 @@ angular.module("HM_ManageJobSitesMD")
       $scope.flags.listView = !$scope.flags.listView;
     }
 
+    $scope.showFilter = false;
+    $scope.filterBtn = function() {
+      $scope.showFilter = !$scope.showFilter
+    }
+
+    $scope.closeFilter = function() {
+        $scope.showFilter = false;
+    }
+
+    $scope.search = function() {
+      $scope.showSearch = !$scope.showSearch;
+      $scope.showFilterDiv = false;
+    }
+
+    $scope.filter = function() {
+      $scope.showFilterDiv = !$scope.showFilterDiv;
+      $scope.showSearch = false;
+    }
+    $scope.reset = function() {
+      $scope.fromDate = '';
+      $scope.toDate = '';
+      $scope.jobsite = '';
+    }
+
   }]);
