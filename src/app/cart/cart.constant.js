3d580ba94cfca0bb786d0979180387466f8984e2
Sanil Naik <sanil.m.naik@gmail.com>
2016-01-16 21:55:45 +0530

    cart progress dynamic

--- src/app/cart/cart.constant.js
+++ src/app/cart/cart.constant.js
@@ -20,5 +20,37 @@ angular.module("HM_CartMD")
       url : function(){
         return '/cart/applyPromocode'
       }
-    }
+    },
+    steps : [
+      {
+        name : 'summary',
+        label : 'CART SUMMARY',
+        od :1,
+        path : 'hm.cart.details',
+      },
+      {
+        name : 'delivery',
+        label : 'DELIVERY DETAILS',
+        od :2,
+        path : 'hm.cart.updateAddress'
+      },
+      {
+        name : 'duration',
+        label : 'DURATION',
+        od :3,
+        path : 'hm.cart.cartDuration'
+      },
+      {
+        name : 'review',
+        label : 'REVIEW ORDER',
+        od :4,
+        path : 'hm.cart.review'
+      },
+      {
+        name : 'payment',
+        label : 'PAYMENT',
+        od :5,
+        path : 'hm.cart.payment'
+      }
+    ]
   });
--- src/app/cart/cart.controller.js
+++ src/app/cart/cart.controller.js
@@ -3,18 +3,42 @@ angular.module("HM_CartMD")
   .controller("HM_CartCtrl",['$scope','toastr','localStorageService','HM_RestSV', 'HM_CartCnst','HM_JobSitesCnst', function( $scope,toastr,localStorageService,RestSV,ShoppingCartCnst,JobSitesCnst ){
 
 
-    $scope.cartData = localStorageService.get('cartData') || {selectedJobSite : {},productsQuantity :{},rentingPeriod : {}};
+    $scope.cartData = localStorageService.get('cartData') || {
+      selectedJobSite : {},
+      productsQuantity :{},
+      rentingPeriod : {},
+      steps : angular.copy(ShoppingCartCnst.steps)
+    };
+
+    var deliveryStep = _.find($scope.cartData.steps, function(o) { return o.name == 'delivery'; });
+    var summaryStep = _.find($scope.cartData.steps, function(o) { return o.name == 'summary'; });
 
     $scope.deleteProductFromCart = deleteProductFromCart;
+
 
     _initialize();
 
     function _initialize(){
       _fetchCart();
       _fetchJobsites();
+
+
       $scope.$watch('cartData', function(cartData){
+        validateStepsCompletion();
         localStorageService.set('cartData',cartData);
       }, true);
+    }
+
+
+    function validateStepsCompletion(){
+      if(!angular.equals($scope.cartData.selectedJobSite, {})){
+        deliveryStep.complete = true;
+      }
+
+      if(!angular.equals($scope.cartData.productsQuantity, {})){
+        summaryStep.complete = true;
+      }
+
     }
 
     function _fetchJobsites(){
@@ -22,6 +46,9 @@ angular.module("HM_CartMD")
         .then(function(response){
           if(response.data.result){
             $scope.cartData.jobsites =  response.data.result.Jobsite_details;
+
+
+
           }
         })
         .catch(function(){
@@ -38,6 +65,7 @@ angular.module("HM_CartMD")
         .then(function(response){
           if(response.data.result){
             $scope.cart = response.data.result.Cart_Content;
+
             $scope.cart && $scope.cart.Product && $scope.cart.Product.forEach(function(product){
               if(!$scope.cartData.productsQuantity[product.Product_id]){
                 $scope.cartData.productsQuantity[product.Product_id] = { qty: product.Product_quantity,isDirty: false}
