c27068b4db17e33a7d78a70312c6d1febcf62b3a
Sanil Naik <sanil.m.naik@gmail.com>
2016-01-17 00:47:29 +0530

    Search page bugs

--- src/app/equipment-detail/equipment-detail.controller.js
+++ src/app/equipment-detail/equipment-detail.controller.js
@@ -1,9 +1,11 @@
 'use strict';
 angular.module("HM_EquipmentDetailMD")
-  .controller("HM_EquipmentDetailCtrl",['$scope','$stateParams',"HM_RestSV","HM_EquipmentDetailCnst", function($scope, $stateParams,RestSV, EquipmentDetailCnst){
+  .controller("HM_EquipmentDetailCtrl",['$scope','$stateParams','$timeout','toastr',"HM_RestSV","HM_EquipmentDetailCnst", function($scope, $stateParams,$timeout,toastr,RestSV, EquipmentDetailCnst){
 
 
     $scope.addToOrRemoveFromWishList = addToOrRemoveFromWishList;
+
+    $scope.calculateDeliveryCharges = calculateDeliveryCharges;
 
     _initialize();
 
@@ -26,12 +28,11 @@ angular.module("HM_EquipmentDetailMD")
       $scope.$broadcast('Add:Wishlist:Process:Start', product.Product_Id);
       if(product.Is_in_catelog){
         RestSV
-          .delete( EquipmentDetailCnst.addToOrRemoveFromWishList.url(),{
-            data : jQuery.param({product_id: product.Product_Id}),
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+          .delete( EquipmentDetailCnst.addToOrRemoveFromWishList.url(),{ data : {product_id : product.Product_Id }})
           .success(function(response){
             product.Is_in_catelog = false;
             $scope.catelog = false;
+            toastr.success("Product removed from catalogue")
           })
           .finally(function(){
            $scope.$broadcast('Add:Wishlist:Process:End',product.Product_Id)
@@ -42,6 +43,7 @@ angular.module("HM_EquipmentDetailMD")
           .success(function(response){
             product.Is_in_catelog = true;
             $scope.catelog = true;
+            toastr.success("Product added to catalogue")
           })
           .finally(function(){
            $scope.$broadcast('Add:Wishlist:Process:End',product.Product_Id)
@@ -49,6 +51,15 @@ angular.module("HM_EquipmentDetailMD")
       }
     }
 
+
+    function calculateDeliveryCharges(){
+      $scope.calculateDeliveryChargesInProgress = true;
+      $timeout(function(){
+        $scope.deliveryRateByZipcode = 90;
+        $scope.calculateDeliveryChargesInProgress = false;
+      },2000)
+
+    }
 
 
   }]);
