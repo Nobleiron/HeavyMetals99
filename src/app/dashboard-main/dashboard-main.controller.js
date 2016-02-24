c3cc56ecd8841b23ade3012640b2da36b599cb27
Sanil Naik <sanil.m.naik@gmail.com>
2016-02-25 02:23:47 +0530

    Dashboard Changes

--- src/app/dashboard-main/dashboard-main.controller.js
+++ src/app/dashboard-main/dashboard-main.controller.js
@@ -1,6 +1,24 @@
 'use strict';
 angular.module('HM_DashboardMD')
   .controller('HM_DashboardMainCtrl', function ($scope) {
+
+
+    _initialize();
+
+
+    function _initialize(){
+      _fetchCatalogCount();
+      _fetchJobsitesCount();
+    }
+
+    function _fetchCatalogCount(){
+
+    }
+
+    function _fetchJobsitesCount(){
+
+    }
+
     $scope.upcomingEquipmentBookings = [{
       name : 'Equipment Name',
       bookedAt : new Date()
