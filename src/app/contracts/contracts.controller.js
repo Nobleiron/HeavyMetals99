f3d85851bd8f7b7eff6de52e9335b479c896757f
ankit <ankit@procoefficient.com>
2016-02-25 20:13:24 +0530

    delivery details page, header alignment, team and board of directors pages, manage jobsite page, invoice page, request and contract pages ui changes

--- src/app/contracts/contracts.controller.js
+++ src/app/contracts/contracts.controller.js
@@ -121,4 +121,20 @@ angular.module("HM_ContractsMD")
       }, $stateParams);
     }
 
+
+    $scope.search = function() {
+      $scope.showSearch = !$scope.showSearch;
+      $scope.showFilterDiv = false;
+    }
+
+    $scope.filter = function() {
+      $scope.showFilterDiv = !$scope.showFilterDiv;
+      $scope.showSearch = false;
+    }
+    $scope.reset = function() {
+      $scope.fromDate = '';
+      $scope.toDate = '';
+      $scope.jobsite = '';
+    }
+
   }]);
